1aa18d6fe68a4084c418ab4e1c10002c419d84ad
Asp Davi Nunes <davinunes.franca@CDS.LOCAL>
2021-09-02 16:54:58 -0300

    Progresso do Curso do Cap Alan na seção

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/aloeclipse/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,3 @@
+
+[
++src/br/eti/amazu/aloeclipse/AloEclipse.java,c\2\c2c4785e9c2cf910a6b775fe4c19095997dc49fd
---
+++ aloeclipse/src/br/eti/amazu/aloeclipse/AloEclipse.java
@@ -0,0 +1,10 @@
+package br.eti.amazu.aloeclipse;
+
+public class AloEclipse {
+
+	public static void main(String[] args) {
+		String mensagem = "Alô Eclipse";
+		System.out.println(mensagem);
+	}
+
+}
